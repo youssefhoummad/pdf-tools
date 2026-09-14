8791665d94ea5ecf54952702cb7df52550f9a2d4
youssef hoummad <youssef.hoummad@outlook.com>
2026-09-14 21:28:54 +0100

    Delete widgets directory

--- widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .infobar import InfoBar
-from .entry import Entry
-from .dnd import apply_dnd
-from .combobox import Combobox
-from .treeview import Treeview
-
-all = ['InfoBar',  'Entry', 'apply_dnd','Combobox', 'Treeview'] #'Pivot',
